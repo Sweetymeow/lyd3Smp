2eebca0a7e0e415ea4096280a441e2f77fcee330
Wendy <honeykatie@qq.com>
2015-02-16 02:25:52 -0500

    add transition animation for height of bar

--- 04_03/start/script.js
+++ 04_03/start/script.js
@@ -1,10 +1,14 @@
+/* Tutor : www.evernote.com/l/AGyZejdx_8tE6KYrg40xsj5mfZVXTAzRrVA */
+
 // var bardata = [20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80];
 
 // More ramdom Data
 var bardata = [];
 for(var i=0; i < 60; i++){
-	bardata.push(Math.random()*30);
+	bardata.push(Math.round(Math.random()*20)+5);
 }
+
+var tempColor;
 
 var height = 400,
     width = 600,
@@ -23,22 +27,49 @@ var xScale = d3.scale.ordinal()
         .domain(d3.range(0, bardata.length))
         .rangeBands([0, width])
 
-d3.select('#chart').append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .style('background', '#C9D7D6')
-    .selectAll('rect').data(bardata)
-    .enter().append('rect')
-        .style('fill', function(d, i){
-	        return colors(i); // Using color scales : Colors
-        })
-        .attr('width', xScale.rangeBand())
-        .attr('height', function(d) {
-            return yScale(d);
-        })
-        .attr('x', function(d,i) {
-            return xScale(i);
-        })
-        .attr('y', function(d) {
-            return height - yScale(d);
-        })
+var mychart = d3.select('#chart').append('svg')
+			    .attr('width', width)
+			    .attr('height', height)
+			    .style('background', '#C9D7D6')
+			    .selectAll('rect').data(bardata)
+			    .enter().append('rect')
+			        .style('fill', function(d, i){
+				        return colors(i); // Using color scales : Colors
+			        })
+			        .attr('width', xScale.rangeBand())
+			        .attr('height', 0)
+			        .attr('x', function(d,i) {
+			            return xScale(i);
+			        })
+			        .attr('y', height) // top-left corner is (0,0) 
+			    .on('mouseover', function(d){
+				    tempColor = this.style.fill;
+				    d3.select(this)
+				    	.transition()
+				    	.style('opacity', .5)
+				    	.style('fill', 'yellow')
+			    })
+			    .on('mouseout', function(d){
+				    d3.select(this)
+				    	.transition()
+				    	.style('opacity', 1)
+				    	.style('fill', tempColor)
+			    })
+mychart.transition()
+	.attr('y', function(d) {
+        return height - yScale(d);
+    })
+    .attr('height', function(d) {
+        return yScale(d);
+    })
+    .delay(function(d, i){
+	    return i*20;
+    })
+    .duration(1000)
+    .ease('elastic');
+
+
+
+
+
+
